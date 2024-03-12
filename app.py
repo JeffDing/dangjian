5a306d7e88a960b5ebc922e254652d84ea209bd4
YiChao Ding <jeffding890430@163.com>
2024-03-12 22:03:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from modelscope import snapshot_download, AutoModel, AutoTokenizer
 import os
 
 def load_chain():
-    model_dir = snapshot_download("JeffDing/dangwu_1_8b", revision="master")
+    model_dir = snapshot_download("Shanghai_AI_Laboratory/internlm2-chat-1_8b", revision="master")
 
     os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
     # 下载模型
