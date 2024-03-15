e593033d159bc72dfedecb0de07f65689d2a9dde
YiChao Ding <jeffding890430@163.com>
2024-03-15 13:27:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def load_chain():
     model_dir = './shizhengwork'
     os.system('apt install git')
     os.system('apt install git-lfs')
+    os.system('git lfs install')
     os.system(f'git clone https://code.openxlab.org.cn/JeffDing/shizhengwork.git {model_dir}')
     os.system(f'cd {model_dir} && git lfs pull')
 
